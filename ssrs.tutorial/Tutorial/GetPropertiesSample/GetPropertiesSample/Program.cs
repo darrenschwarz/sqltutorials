0ca48dae42bdfaa494a6c5093cd4395b582e6841
darrenschwarz <darrenschwarz@gmail.com>
2015-11-27 14:43:39 +0000

    Creating the Web Service Client Project Lessons 3

--- ssrs.tutorial/Tutorial/GetPropertiesSample/GetPropertiesSample/Program.cs
+++ ssrs.tutorial/Tutorial/GetPropertiesSample/GetPropertiesSample/Program.cs
@@ -1,8 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using GetPropertiesSample.ReportService2010;
 
 namespace GetPropertiesSample
 {
@@ -10,6 +7,37 @@ namespace GetPropertiesSample
     {
         static void Main(string[] args)
         {
+            ReportingService2010 rs = new ReportingService2010();
+            rs.Credentials = System.Net.CredentialCache.DefaultCredentials;
+            rs.Url = "http://thebeast/ReportServer_SQL/reportservice2010.asmx";
+
+            Property name = new Property();
+            name.Name = "Name";
+
+            Property description = new Property();
+            description.Name = "Description";
+
+            Property[] properties = new Property[2];
+            properties[0] = name;
+            properties[1] = description;
+
+            try
+            {
+
+                Property[] returnProperties = rs.GetProperties("/Tutorial/SalesOrders", properties);
+                
+                foreach (Property p in returnProperties)
+                {
+                    Console.WriteLine(p.Name + ": " + p.Value);
+                }
+
+                Console.ReadLine();
+            }
+
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
         }
     }
 }
